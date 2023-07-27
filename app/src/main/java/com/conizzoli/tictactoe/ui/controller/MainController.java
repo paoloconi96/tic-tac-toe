6262a0edda275876a379f95d6b8df973341ebfa2
Paolo Conizzoli <paolo.conizzoli@gmail.com>
2023-07-27 13:28:11 +0200

    Introduced dependecy injection using Guice library (#4)

--- app/src/main/java/com/conizzoli/tictactoe/ui/controller/MainController.java
+++ app/src/main/java/com/conizzoli/tictactoe/ui/controller/MainController.java
@@ -2,24 +2,31 @@ package com.conizzoli.tictactoe.ui.controller;
 
 import com.conizzoli.tictactoe.engine.exception.GameBoardLocationCouldNotBeMarkedBecausePlayerIsNotNextMover;
 import com.conizzoli.tictactoe.menu.MenuAction;
+import com.google.inject.Inject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class MainController {
-    private final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    private final ResourceBundle resourceBundle = ResourceBundle.getBundle("TicTacToeBundle");
-    private final GameController gameController = new GameController();
+    private final BufferedReader bufferedReader;
+    private final ResourceBundle resourceBundle;
+    private final GameController gameController;
     private final HashMap<String, MenuAction> menuActionHashMap = new HashMap<>();
     {
         menuActionHashMap.put("1", MenuAction.NEW_GAME);
         menuActionHashMap.put("2", MenuAction.RESUME_GAME);
         menuActionHashMap.put("3", MenuAction.ASSIGN_PLAYER_NAMES);
         menuActionHashMap.put("0", MenuAction.EXIT);
+    }
+
+    @Inject
+    public MainController(BufferedReader bufferedReader, ResourceBundle resourceBundle, GameController gameController) {
+        this.bufferedReader = bufferedReader;
+        this.resourceBundle = resourceBundle;
+        this.gameController = gameController;
     }
 
     public void invoke() throws GameBoardLocationCouldNotBeMarkedBecausePlayerIsNotNextMover, IOException {
